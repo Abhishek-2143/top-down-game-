c0ee0fa719b17b9e2b45b25bb8414196e7757c50
Abhishek Kumar Gupta <abhishekkumargupta2143@gmail.com>
2024-08-09 16:02:08 +0530

    movement with speed

--- Assets/scripts/Movement Script/PlayerMovement.cs
+++ Assets/scripts/Movement Script/PlayerMovement.cs
@@ -5,11 +5,17 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    
+    Vector2 MovementValue = Vector2.zero;
+    public float movementSpeed = 0.5f;
+    public float forceAmount = 1;
+    public float _mass = 20;
+
+    public Rigidbody Rb_Body;
     public void IAAccelerate(InputAction.CallbackContext context)
     {
-        Vector2 MoveValue = context.ReadValue<Vector2>();//<vector2> is a carats
-        Debug.Log (MoveValue); 
-        transform.Translate(MoveValue.x, 0, MoveValue.y);
+        MovementValue = context.ReadValue<Vector2>();//<vector2> is a carats
+        
     }
     
     // Start is called before the first frame update
@@ -17,11 +23,16 @@ public class PlayerMovement : MonoBehaviour
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
-        
+
+        //transform.Translate(MovementValue.x * movementSpeed * Time.deltaTime, 0, MovementValue.y * movementSpeed * Time.deltaTime);//time.deltaTime is time take to r
+        Rb_Body.AddForce(MovementValue.x * movementSpeed * Time.deltaTime, 0, MovementValue.y * movementSpeed * Time.deltaTime);
+        Rb_Body.mass = _mass;
     }
-   
+    
+
+  
 }
